5cd33ac3969947852976f95e576f0dfd39103ac1
Denis Arkhipenko <denis-arhipenko@mail.ru>
2017-11-10 00:01:21 +0500

    Update html.c

--- html.c
+++ html.c
@@ -858,7 +858,7 @@ write_standings_header(const serve_state_t state,
     if (header_str) {
       process_template(f, header_str, 0, charset, header, 0);
     } else {
-      fprintf(f, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=%s\"/><title>%s</title><script src=\"ejudge/standings.js\"></script><link rel=\"stylesheet\" type=\"text/css\" href=\"ejudge/standings.css\"></head><body><h1>%s</h1>",
+      fprintf(f, "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=%s\"/><title>%s</title><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js\"></script><script src=\"ejudge/standings.js\"></script><link rel=\"stylesheet\" type=\"text/css\" href=\"ejudge/standings.css\"></head><body><h1>%s</h1>",
               charset,
               header, header);
     }
